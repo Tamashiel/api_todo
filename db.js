fcef2d99f6491418c8d11e11cba057a2cfaf0a9c
unknown <aguirregonzalezsonia@gmail.com>
2024-11-18 12:42:53 +0100

    api lista

--- db.js
+++ db.js
@@ -1,60 +1,119 @@
-import dotenv from "dotenv";
-dotenv.config(); //lee el fichero .env y crea las variables de entorno
+//Cuando hemos terminado con la parte de la base de datos, el dotenv ya no lo necesitaremos porque lo tendremos en el index.js y este importará todas las funciones que tenemos en la base de datos, por lo cual aunque podemos dejarlo repetido y no pasa nada, es una buena acción el eliminarlo.
+
+//import dotenv from "dotenv";
+//dotenv.config(); //lee el fichero .env y crea las variables de entorno
 //--------------
 import postgres from "postgres";
 
-function conectar(){ //conectamos postgres usando .env para que la información sensible no se traspase.
+function conectar(){
     return postgres({
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD
+        host : process.env.DB_HOST,
+        database : process.env.DB_NAME,
+        user : process.env.DB_USER,
+        password : process.env.DB_PASSWORD 
     });
 }
 
 export function leerTareas(){
-    return new Promise(async (ok, ko) => {
+    return new Promise(async (ok,ko) => {
         let conexion = conectar();
+
         try{
-            let tareas = await conexion`SELECT * FROM tareas`; //tiramos la consulta y la respuesta es una lectura
+
+            let tareas = await conexion`SELECT * FROM tareas ORDER BY id`;
+
             conexion.end();
+
             ok(tareas);
+
         }catch(error){
-            ko({ error : "error en la base de datos" });
+
+            ko({ error : "error bbdd" });
+
         }
     });
 }
 
 export function crearTarea(tarea){
-    return new Promise(async (ok, ko) => {
+    return new Promise(async (ok,ko) => {
         let conexion = conectar();
+
         try{
-            let [{id}] = await conexion`INSERT INTO tareas (tarea) VALUES (${tarea}) RETURNING id`; 
+
+            let [{id}] = await conexion`INSERT INTO tareas (tarea) VALUES (${tarea}) RETURNING id`;
+
             conexion.end();
+
             ok(id);
+
         }catch(error){
-            ko({ error : "error en la base de datos" });
+
+            ko({ error : "error bbdd" });
+
         }
     });
 }
 
 export function borrarTarea(id){
-    return new Promise(async (ok, ko) => {
+    return new Promise(async (ok,ko) => {
         let conexion = conectar();
+
         try{
-            let {count} = await conexion `DELETE FROM tareas WHERE id = ${id}`;
+
+            let {count} = await conexion`DELETE FROM tareas WHERE id = ${id}`;
+
             conexion.end();
+
             ok(count);
+
         }catch(error){
-            ko({ error : "error en la base de datos" });
+            
+            ko({ error : "error bbdd" });
+
         }
     });
 }
 
-leerTareas()
-.then(x => console.log(x))
-.catch(x => console.log(x))
-/*
-borrarTarea("Termina API")
-.then(x => console.log(x))
-.catch(x => console.log(x));*/
+export function editarTarea(id,texto){
+    return new Promise(async (ok,ko) => {
+        let conexion = conectar();
+
+        try{
+
+            let {count} = await conexion`UPDATE tareas SET tarea = ${texto} WHERE id = ${id}`;
+
+            conexion.end();
+
+            ok(count);
+
+        }catch(error){
+            
+            ko({ error : "error bbdd" });
+
+        }
+    });
+}
+
+export function editarEstado(id){
+    return new Promise(async (ok,ko) => {
+        let conexion = conectar();
+
+        try{
+
+            let {count} = await conexion`UPDATE tareas SET estado = NOT estado WHERE id = ${id}`;
+
+            conexion.end();
+
+            ok(count);
+
+        }catch(error){
+            
+            ko({ error : "error bbdd" });
+
+        }
+    });
+}
+
+
+
+
